06fce35dc47934fb2ad28a2261de322bc794aa72
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-07-31 00:42:01 -0700

    Add in basic configurations, classes, and attributes

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 	require('./functions.php');
 	config();
+	define_ua();
 ?>
 <!doctype html>
-<html>
+<!--[if lt IE 7]>      <html class="lt-ie9 lt-ie8 lt-ie7 no-js"> <![endif]-->
+<!--[if IE 7]>         <html class="lt-ie9 lt-ie8 no-js"> <![endif]-->
+<!--[if IE 8]>         <html class="lt-ie9 no-js"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" > <!--<![endif]-->
 	<?php load('head')?>
-	<body>
+	<body lang="en">
 		<?php load('header', 'main', 'footer')?>
 	</body>
 </html>
