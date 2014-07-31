feb42e2031108602fccfd475e43150bc18cee905
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-07-31 00:42:29 -0700

    Get info for head from database and determine stylsheets to use

--- components/head.php
+++ components/head.php
@@ -4,9 +4,18 @@
 <head>
 	<meta charset="utf-8"/>
 	<title><?=$head->title?></title>
-	<link rel="stylesheet" type="text/css" href="./stylesheets/style.css" media="all"/>
-	<!--<link rel="stylesheet" type="text/css" href="./stylesheets/normalize.css" media="all"/>-->
-	<script type="application/javascript" src="./scripts/functions.js" defer></script>
+	<meta name="description" content="<?=$head->description?>"/>
+	<meta name="keywords" content="<?=$head->keywords?>"/>
+	<meta name="robots" content="<?=$head->robots?>"/>
+	<meta name="viewport" content="<?=$head->viewport?>"/>
+	<base href="<?=URL?>/"/>
+	<?php if(localhost() and BROWSER === 'Firefox'):?>
+	<link rel="stylesheet" type="text/css" href="stylesheets/normalize.css" media="all"/>
+	<link rel="stylesheet" type="text/css" href="stylesheets/style.css" media="all"/>
+	<?php else:?>
+	<link rel="stylesheet" type="text/css" href="stylesheets/combined.out.css" media="all"/>
+	<?php endif?>
+	<script type="application/javascript" src="scripts/functions.js" defer></script>
 	<!--[if lte IE 8]>
 		<script type="text/javascript">
 			var html5=new Array('header','hgroup','nav','menu','main','section','article','footer','aside','mark');
