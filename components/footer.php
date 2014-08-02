3bc8d54a47b229f94cf8a42f3bc29b33522210a4
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-08-02 12:50:19 -0700

    Moved contact info to separate file. Now uses database

--- components/footer.php
+++ components/footer.php
@@ -1,30 +1,5 @@
 <footer role="contentinfo">
-	<address itemtype="http://schema.org/Person" itemscope>
-		<div>
-			<div>
-				<span itemprop="name">
-					<b itemprop="givenName">Ryan</b>
-					<b itemprop="familyName">Tall</b><br />
-				</span>
-				<i itemprop="jobTitle">Solar Consultant</i>
-				- <b itemprop="worksFor">
-					<a itemprop="url" title="VivintSolar Homepage" href="http://vivintsolar.com">vivintsolar</a>
-				</b>
-			</div><br />
-			<div>
-				<a href="tel:6618331977" target="_blank" title="Call the office" itemprop="telephone">Office: 661.833.1977 <?php include('images/icons/telephone.svg')?></a><br />
-				<a href="tel:6614345324" target="_blank" title="Call my cell phone" itemprop="telephone">Cell: 661.434.5324 <?php include('images/icons/mobile_icon.svg')?></a><br />
-				<a href="mailto:rtall@vivintsolar.com" target="_blank" title="Send me an email" itemprop="email">rtall@vivintsolar.com <?php include('images/icons/envelope.svg')?></a><br /><br />
-			</div>
-		</div>
-		<div itemprop="workLocation" itemscope itemtype="http://schema.org/PostalAddress">
-			<span itemprop="streetAddress">6801 McDivitt Dr.<br />Suite A</span><br />
-			<span itemprop="addressLocality">Bakersfield</span>,
-			<span itemprop="addressRegion">CA</span>
-			<span itemprop="postalCode">93313</span><br />
-		</div>
-		<br /><br />
-	</address>
+	<?php load('contact')?>
 	<br />
 	<hr />
 	<p>&copy; <?=date('Y')?></p>
