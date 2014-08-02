b11b29f9b528545a31f5f430dcea346c98f170f4
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-08-01 21:29:02 -0700

    Remove hgroup
    I guess it is depreciated...
    
    Makes for a smaller :hover target, but can work around it.

--- components/header.php
+++ components/header.php
@@ -1,10 +1,8 @@
-<header>
-	<hgroup>
-		<h1>
-			<a href="http://www.vivintsolar.com/en/">vivint solar</a>
-		</h1>
-		<h2>Hello World!</h2>
-	</hgroup>
+<header role="banner">
+	<h1>
+		<a href="http://www.vivintsolar.com/en/">vivint solar</a>
+	</h1>
+	<h2>Hello World!</h2>
 	<picture>
 		<source media="(min-width: 1400px)" srcset="images/1600.jpg"/>
 		<source media="(min-width: 1000px)" srcset="images/1200.jpg"/>
