6ebd339f3d268c2b6f308c6ea8ee32d4915ad19a
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-08-01 21:27:19 -0700

    Add contact info with micro-data

--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,28 @@
-<footer>
-	&copy; <?=date('Y')?>
-	<a href="mailto:rtall@vivintsolar.com" target="_blank" title="Send me an email"><?php include('images/envelope.svg')?></a>
+<footer role="contentinfo">
+	<address itemtype="http://schema.org/Person" itemscope>
+		<div>
+			<div>
+				<span itemprop="name">
+					<b itemprop="givenName">Ryan</b>
+					<b itemprop="familyName">Tall</b><br />
+				</span>
+				<i itemprop="jobTitle">Solar Consultant</i>
+				- <b itemprop="worksFor">
+					<a itemprop="url" href="http://vivintsolar.com">vivintsolar</a>
+				</b>
+			</div><hr />
+			<div>
+				<a href="tel:6618331977" target="_blank" itemprop="telephone">Office: 661.833.1977 <?php include('images/telephone.svg')?></a><br />
+				<a href="tel:6614345324" target="_blank" itemprop="telephone">Cell: 661.434.5324 <?php include('images/mobile_icon.svg')?></a><br />
+				<a href="mailto:rtall@vivintsolar.com" target="_blank" title="Send me an email" itemprop="email">rtall@vivintsolar.com <?php include('images/envelope.svg')?></a><br /><br />
+			</div>
+		</div>
+		<div itemprop="workLocation" itemscope itemtype="http://schema.org/PostalAddress">
+			<span itemprop="streetAddress">6801 McDivitt Dr.<br />Suite A</span><br />
+			<span itemprop="addressLocality">Bakersfield</span>,
+			<span itemprop="addressRegion">CA</span>
+			<span itemprop="postalCode">93313</span><br />
+		</div>
+	</address>
+	<p>&copy; <?=date('Y')?></p>
 </footer>
