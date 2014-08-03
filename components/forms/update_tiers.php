383925a7bf0685cb49624e97828217b2f7d6d03a
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-08-02 22:44:56 -0700

    Created form for updating tier rate info

---
+++ components/forms/update_tiers.php
@@ -0,0 +1,38 @@
+<?php
+	$tiers = $DB->fetch_array("
+		SELECT `tier`, `rate`, `kw`
+		FROM `tiers`
+		ORDER BY `tier`
+	");
+
+?>
+<form name="<?=filename(__FILE__)?>" action="." method="post">
+	<table>
+		<caption>Tier Rates</caption>
+		<thead>
+			<tr>
+				<th>Tier</th>
+				<th>Rate</th>
+				<th>Starting Kilowatts</th>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<th>Tier</th>
+				<th>Rate</th>
+				<th>Starting Kilowatts</th>
+			</tr>
+		</tfoot>
+		<tbody>
+		<?php foreach($tiers as $tier):?>
+			<tr>
+				<td><?=$tier->tier?></td>
+				<td><input type="number" min="0" step="0.01" name="rate[<?=$tier->tier?>]" value="<?=$tier->rate?>" required/></td>
+				<td><input type="number" min="0" step="1" name="kw[<?=$tier->tier?>]" value="<?=$tier->kw?>" required/></td>
+			</tr>
+		<?php endforeach?>
+		</tbody>
+	</table>
+	<input type="submit" value="Submit"/>
+	<input type="reset" value="Reset"/>
+</form>
