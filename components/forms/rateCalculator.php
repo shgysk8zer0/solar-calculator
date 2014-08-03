bdfeca32922bb28d425ef401f903539c4e51dc1c
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-08-02 23:30:23 -0700

    Updated estimation notice

--- components/forms/rateCalculator.php
+++ components/forms/rateCalculator.php
@@ -9,10 +9,10 @@
 <form name="<?=filename(__FILE__)?>">
 	<label for="in">How much was your last electric bill?</label>
 	$<input type="number" id="in" min="0" max="1000" step="0.01" name="paidRate" pattern="\d(\.\d{1,2})?" placeholder="117.42" autofocus/><br />
-	Wouldn't you rather pay this? $<output for="paidRate" name="calculated" id="out">0.00</output><b>*</b> <br />
+	Wouldn't you rather pay this? $<output for="paidRate" name="calculated" id="out">0.00</output><sup><b>*</b></sup> <br />
 	<small>
 		<b>*</b>
-		<em>Rates are estimated.</em>
+		<em>Rates are estimated.<br /> As electric bills are based on a number of factors, including time of day, it is not possible to provide accurate results.</em>
 	</small>
 	<input type="hidden" name="solarRate" value="0.15"/>
 	<?php foreach($tiers as $tier):?>
