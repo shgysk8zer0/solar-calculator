94b03aae75fe8e132cd4c44880a009a4010e97e7
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-08-04 18:58:46 -0700

    Inverted <source> order and set min/max width except on extremes
    Should still work the same, but possibly better.
    
    There isn't much about the element, and I've seen best-first as well as
    best-fit last. This should only ever match one <source>, so it shouldn't
    matter.

--- components/header.php
+++ components/header.php
@@ -4,10 +4,14 @@
 	</h1>
 	<h2>Hello World!</h2>
 	<picture>
+		<source media="(max-width: 599px)" srcset="images/banners/400.jpg"/>
+		<source media="(min-width: 600px) and (max-width: 999px)" srcset="images/banners/800.jpg"/>
+		<source media="(min-width: 1000px) and (max-width: 1399px)" srcset="images/banners/1200.jpg"/>
 		<source media="(min-width: 1400px)" srcset="images/banners/1600.jpg"/>
+		<!--<source media="(min-width: 1400px)" srcset="images/banners/1600.jpg"/>
 		<source media="(min-width: 1000px)" srcset="images/banners/1200.jpg"/>
 		<source media="(min-width: 600px)" srcset="images/banners/800.jpg"/>
-		<source media="(max-width: 599px)" srcset="images/banners/400.jpg"/>
+		<source media="(max-width: 599px)" srcset="images/banners/400.jpg"/>-->
 		<img src="" alt="Solar Family"/>
 	</picture>
 </header>
